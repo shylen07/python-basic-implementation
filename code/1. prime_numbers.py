cb4aa7cd6d0326157199985e4e293257fe6eeba7
Devender Singh <dkathait269@gmail.com>
2023-06-13 10:06:43 +0530

    Python Practice

---
+++ code/1. prime_numbers.py
@@ -0,0 +1,76 @@
+# Title: Prime Number Checker
+# Created by Devender Singh
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+number = int(input("Enter a number: "))
+if is_prime(number):
+    print("Prime")
+else:
+    print("Not Prime")
+
+# Commit on 2023-04-22 00:00:00 +0530
+
+# Commit on 2023-04-23 00:00:00 +0530
+
+# Commit on 2023-04-24 00:00:00 +0530
+
+# Commit on 2023-04-26 00:00:00 +0530
+
+# Commit on 2023-04-27 00:00:00 +0530
+
+# Commit on 2023-04-28 00:00:00 +0530
+
+# Commit on 2023-04-29 00:00:00 +0530
+
+# Commit on 2023-04-30 00:00:00 +0530
+
+# Commit on 2023-05-02 00:00:00 +0530
+
+# Commit on 2023-05-03 00:00:00 +0530
+
+# Commit on 2023-05-04 00:00:00 +0530
+
+# Commit on 2023-05-05 00:00:00 +0530
+
+# Commit on 2023-05-06 00:00:00 +0530
+
+# Commit on 2023-05-07 00:00:00 +0530
+
+# Commit on 2023-05-09 00:00:00 +0530
+
+# Commit on 2023-05-10 00:00:00 +0530
+
+# Commit on 2023-05-11 00:00:00 +0530
+
+# Commit on 2023-05-12 00:00:00 +0530
+
+# Commit on 2023-05-13 00:00:00 +0530
+
+# Commit on 2023-05-15 00:00:00 +0530
+
+# Commit on 2023-05-16 00:00:00 +0530
+
+# Commit on 2023-04-22 00:00:00 +0530
+
+# Commit on 2023-04-23 00:00:00 +0530
+
+# Commit on 2023-04-22 00:00:00 +0530
+
+# Commit on 2023-04-23 00:00:00 +0530
+
+# Commit on 2023-04-24 00:00:00 +0530
+
+# Commit on 2023-04-26 00:00:00 +0530
+
+# Commit on 2023-04-27 00:00:00 +0530
+
+# Commit on 2023-04-28 00:00:00 +0530
+
+# Commit on 2023-04-29 00:00:00 +0530
