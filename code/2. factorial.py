a04ad827f17b5d6689e6bd1716609d8465f3b9d0
Devender Singh <dkathait269@gmail.com>
2023-06-13 10:11:01 +0530

    Python Practice

---
+++ code/2. factorial.py
@@ -0,0 +1,65 @@
+# Title: Factorial Calculator
+# Created by Devender Singh
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+number = int(input("Enter a number: "))
+print("Factorial:", factorial(number))
+
+# Commit on 2023-04-22 00:00:00 +0530
+
+# Commit on 2023-04-23 00:00:00 +0530
+
+# Commit on 2023-04-24 00:00:00 +0530
+
+# Commit on 2023-04-26 00:00:00 +0530
+
+# Commit on 2023-04-27 00:00:00 +0530
+
+# Commit on 2023-04-28 00:00:00 +0530
+
+# Commit on 2023-04-29 00:00:00 +0530
+
+# Commit on 2023-04-30 00:00:00 +0530
+
+# Commit on 2023-05-02 00:00:00 +0530
+
+# Commit on 2023-05-03 00:00:00 +0530
+
+# Commit on 2023-05-04 00:00:00 +0530
+
+# Commit on 2023-05-05 00:00:00 +0530
+
+# Commit on 2023-05-06 00:00:00 +0530
+
+# Commit on 2023-05-07 00:00:00 +0530
+
+# Commit on 2023-05-09 00:00:00 +0530
+
+# Commit on 2023-05-10 00:00:00 +0530
+
+# Commit on 2023-05-11 00:00:00 +0530
+
+# Commit on 2023-05-12 00:00:00 +0530
+
+# Commit on 2023-05-13 00:00:00 +0530
+
+# Commit on 2023-05-15 00:00:00 +0530
+
+# Commit on 2023-04-22 00:00:00 +0530
+
+# Commit on 2023-04-22 00:00:00 +0530
+
+# Commit on 2023-04-23 00:00:00 +0530
+
+# Commit on 2023-04-24 00:00:00 +0530
+
+# Commit on 2023-04-26 00:00:00 +0530
+
+# Commit on 2023-04-27 00:00:00 +0530
+
+# Commit on 2023-04-28 00:00:00 +0530
