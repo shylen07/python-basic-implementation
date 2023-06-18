eee43d7499ba30312bf1298a69688b9e0671926c
Devender Singh <dkathait269@gmail.com>
2023-06-18 10:23:17 +0530

    Python Practice

---
+++ code/10. pattern.py
@@ -0,0 +1,9 @@
+# Title: Pattern Printing
+# Created by Devender Singh
+
+def print_pattern(n):
+    for i in range(1, n + 1):
+        print("*" * i)
+
+rows = int(input("Enter the number of rows: "))
+print_pattern(rows)
